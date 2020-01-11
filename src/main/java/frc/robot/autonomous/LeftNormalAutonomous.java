45e2f7b6c9cc1e2b31957df352f7bc99fb56b241
Mira Eisenhauer <vz.ceti.e@gmail.com>
2020-01-11 17:24:53 -0500

    New Left Normal Autonomous

--- src/main/java/frc/robot/autonomous/LeftNormalAutonomous.java
+++ src/main/java/frc/robot/autonomous/LeftNormalAutonomous.java
@@ -4,7 +4,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 // import edu.wpi.first.wpilibj.command.WaitCommand;
 import frc.robot.commands.*;
 
-public class CenterNormalAutonomous extends CommandGroup {
+public class LeftNormalAutonomous extends CommandGroup {
 
   public final double robotX = 34; //robotX is full width of robot including bumpers (left to right)
   public final double robotY = 38.25;  //robotY is full length of robot including bumpers (front to back)
@@ -14,16 +14,14 @@ public class CenterNormalAutonomous extends CommandGroup {
   public double timeout = 8; //replace with actual value
   public double speed = 0.8;
 
-  public CenterNormalAutonomous() {
+  public LeftNormalAutonomous() {
     if (crossLine > 82.625+robotY) {
       //((field length minus 2*(sector depth) minus (trench run depth))/2) -4 (amount by which dimensions can vary)
       //to prevent us from going too far back
       addSequential(new DriveStraight(robotY, speed, true, timeout)); //DO NOT CHANGE
     } else {
       addSequential(new DriveStraight(crossLine, speed, true, timeout));
-      addSequential(new Turn(-90, speed, timeout));
-      addSequential(new DriveStraight(distanceToGoalX, speed, true, timeout));
-      addSequential(new Turn(-90, speed, timeout));   
+      addSequential(new Turn(180, speed, timeout));
       addSequential(new DriveStraight(distanceToGoalY, speed, true, timeout));
       //addSequential(new Shoot()); //shoot
       //addSequential(new DriveStraight()); //go to intake more balls
