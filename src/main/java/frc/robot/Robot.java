693889a5974013424cf54b2684ddc6c7d489ce47
Dendendelen <d.y.plotnikov@gmail.com>
2020-01-11 16:25:17 -0500

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,11 +1,26 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-// import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import frc.robot.subsystems.*;
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTable;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+
 
 public class Robot extends TimedRobot {
+
+    private UsbCamera camera;
+
+    private NetworkTableEntry bigger;
+    /*private NetworkTableEntry lineX1;
+    private NetworkTableEntry lineY0;
+    private NetworkTableEntry lineY1;*/
 
     public Robot() {
         super(0.06);
@@ -13,6 +28,14 @@ public class Robot extends TimedRobot {
 
     public void robotInit() {
         System.out.println("Initializing " + RobotMap.botName + "\n");
+
+        camera = CameraServer.getInstance().startAutomaticCapture();
+        
+        NetworkTableInstance inst = NetworkTableInstance.getDefault();
+        NetworkTable pie = inst.getTable("pie");
+        bigger = pie.getEntry("bigger");
+       
+
         Subsystems.driveBase.cheesyDrive.setSafetyEnabled(false);
     }
 
@@ -46,5 +69,10 @@ public class Robot extends TimedRobot {
         printDataToSmartDashboard();
     }
 
-    private void printDataToSmartDashboard() {}
+    private void printDataToSmartDashboard() {
+
+        SmartDashboard.putNumber("bigger", bigger.getDouble(-404));
+               
+
+    }
 }
