9ec7fecc925612ce47dde96765ea3dc3008eb0b8
Margaret Earnest <molly7312000@gmail.com>
2020-01-24 19:29:45 -0500

    Updated ports (w/ comp bot)

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,87 +1,101 @@
 package frc.robot;
 
+/**
+ * Ports for motor controllers, caps for speed and rotation, and turn direction.
+ */
 public class RobotMap {
 
-    public static String botName = "Practice Bot";
-    public static double idealAngle = 0;
-    public static double driveOffset = 0;
-    public static double turnDirection = 0;
     public static double speedCap = 0.25;
     public static double rotationCap = 0.5;
-    
+    public static double wheelDiameter;
+
+    // Drive base ports
+
+    public static int leftFrontFollower;
+    public static int leftMiddleMaster;
+    public static int leftRearFollower;
+    public static int rightFrontFollower;
+    public static int rightMiddleMaster;
+    public static int rightRearFollower;
+
+    public enum BotNames {
+        COMPETITION, PRACTICE, TOASTER
+    }
+
     /**
-     * Various Ports
+     * <p><code>"toaster"</code>, <code>"practice"</code>, or <code>"comp"</code>.</p>
+     * Please note that this is case sensitive.
      */
-    
-    // Talon/Victor IDs
-    //VALUES FOR TOASTER
-    // public static final int leftFrontFollower = 3;
-    // public static final int leftMiddleMaster = 2;
-    // public static final int leftRearFollower = 6;
-    // public static final int rightFrontFollower = 5;
-    // public static final int rightMiddleMaster = 14;
-    // public static final int rightRearFollower = 4;
+    public static BotNames botName = BotNames.COMPETITION;
 
-    public static final boolean isCompBot = true;
+    /**
+     * Sets the bot's ports based off of the bot's name. You will still have to comment out code in DriveBase.java that determines whether your motors are victors or talons.
+     * @param bot The name of the bot. <code>"toaster"</code>, <code>"practice"</code>, or <code>"comp"</code>.
+     */
+    public static void setBot(BotNames bot) {
+        botName = bot;
+        if (bot == BotNames.TOASTER) {
+            leftFrontFollower = 43;
+            leftMiddleMaster = 18;
+            leftRearFollower = 2;
+            rightFrontFollower = 60;
+            rightMiddleMaster = 14;
+            rightRearFollower = 57;
 
-    public static final int rightRearFollower = (isCompBot) ? 45 : 20;
-    public static final int rightMiddleMaster = (isCompBot) ? 10 : 26; 
-    public static final int rightFrontFollower = (isCompBot) ? 1 : 9; 
-    public static final int leftRearFollower = (isCompBot) ? 45 : 31;
-    public static final int leftMiddleMaster = (isCompBot) ? 19 : 34;
-    public static final int leftFrontFollower = (isCompBot) ? 7 : 35;
-    
-    
-    // Double Solenoid Values (PCM 0)
+            wheelDiameter = 8;
+        } else if (bot == BotNames.PRACTICE) {
+            leftFrontFollower = 3;
+            leftMiddleMaster = 20;
+            leftRearFollower = 6;
+            rightFrontFollower = 5;
+            rightMiddleMaster = 31;
+            rightRearFollower = 4;
 
-    // Double Solenoid Values (PCM 1)
+            wheelDiameter = 6;
+        } else if (bot == BotNames.COMPETITION) {
+            leftFrontFollower = 7;
+            leftMiddleMaster = 29;
+            leftRearFollower = 8;
+            rightFrontFollower = 9;
+            rightMiddleMaster = 10;
+            rightRearFollower = 45;
 
-    // Digital IO Ports
-    
+            wheelDiameter = 6;
+        }
+    }
+
+    // Subsystem motor controller ports (Talons/Victors)
+
+    public static final int leftFlywheel = 11;
+    public static final int rightFlywheel = 24;
+
     // UI Ports
-    public static final int launchpad = 0;
+
     public static final int driverXboxController = 1;
     public static final int operatorXboxController = 2;
+
     /**
-     * End Port Setting
+     * @return The speed cap for the drive base in teleop.
      */
-   
-    public static double getIdealAngle() {
-        return idealAngle;
-    }
-    
-    public static void setIdealAngle(double angle) {
-        idealAngle = angle;
-        System.out.println("Ideal angle is now " + idealAngle);
-    }
-    
     public static double getSpeedCap() {
         return speedCap;
     }
-    
+
+    /**
+     * @return The rotation speed cap for the drive base in teleop.
+     */
     public static double getRotationCap() {
         return rotationCap;
     }
-    
-    public static void setSpeedAndRotationCaps(double newSpeedCap, double newRotationCap) {
+
+    /**
+     * Sets the caps on speed & rotation for the drive base in teleop.
+     * @param newSpeedCap The speed cap to set.
+     * @param newRotationCap The rotation speed cap to set.
+     */
+    public static void setSpeedAndRotationCaps(final double newSpeedCap, final double newRotationCap) {
         speedCap = newSpeedCap;
         rotationCap = newRotationCap;
     }
-    
-    public static void setDriveOffset(double offset) {
-        driveOffset = offset;
-        System.out.println("Drive offset is now " + driveOffset);
-    }
-    
-    public static double getDriveOffset() {
-        return driveOffset;
-    }
-    
-    public static double getTurnDirection() {
-        return turnDirection;
-    }
-    
-    public static void setTurnDirection(double direction) {
-        turnDirection = direction;
-    }
+
 }
--- src/main/java/frc/robot/commands/DriveStraight.java
+++ src/main/java/frc/robot/commands/DriveStraight.java
@@ -1,21 +1,31 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.RobotMap;
 import frc.robot.subsystems.Subsystems;
 
+/**
+ * A Command to drive the robot in a straight line.
+ */
 public class DriveStraight extends Command {
 
     private double ticks;
     private boolean forward;
     private double speed;
-    private boolean hasZeroed = false;
 
-    public DriveStraight(double inches, double Speed, double Timeout) {
+    /**
+     * Drives the robot in a straight line.
+     * @param Inches Distance forwards or backwards in inches.
+     * @param Speed Speed at which the bot travels (0 to 1).
+     * @param Timeout The timeout, in seconds.
+     */
+    public DriveStraight(double Inches, double Speed, double Timeout) {
         super("DriveStraight");
         requires(Subsystems.driveBase);
-        ticks = convertToTicks(Math.abs(inches));
-        forward = inches > 0;
+        ticks = convertToTicks(Math.abs(Inches));
+        forward = Inches > 0;
         speed = Speed;
+        setTimeout(Timeout);
     }
 
     @Override
@@ -27,17 +37,9 @@ public class DriveStraight extends Command {
 
     @Override
     public void execute() {
-        if (!hasZeroed) {
-            if (Math.abs(Subsystems.driveBase.getLeftPosition()) < 150 && Math.abs(Subsystems.driveBase.getRightPosition()) < 150) {
-              hasZeroed = true;
-            } else {
-              return;
-            }
-        }
-          
-        double correction = Subsystems.driveBase.getGyroAngle();
-        correction *= 0.075;
-        correction += 1.0;
+        // double correction = Subsystems.driveBase.getGyroAngle();
+        // correction *= 0.075;
+        // correction += 1.0;
         if (forward) {
             Subsystems.driveBase.setMotors(-speed, -speed);
         } else {
@@ -47,12 +49,8 @@ public class DriveStraight extends Command {
 
     @Override
     public boolean isFinished() {
-        if (!hasZeroed) {
-            return false;
-        }
-          
-        int leftPosition = (int) Math.abs(Subsystems.driveBase.getLeftPosition());
-        int rightPosition = (int) Math.abs(Subsystems.driveBase.getRightPosition());
+        int leftPosition = Math.abs(Subsystems.driveBase.getLeftPosition());
+        int rightPosition = Math.abs(Subsystems.driveBase.getRightPosition());
         return (leftPosition > ticks) || (rightPosition > ticks) || isTimedOut();
     }
 
@@ -61,14 +59,17 @@ public class DriveStraight extends Command {
         Subsystems.driveBase.setMotors(0,0);
     }
 
-
     @Override
     public void end() {
         Subsystems.driveBase.setMotors(0,0);
     }
 
+    /**
+     * @param inches Inches to convert.
+     * @return The equivalent distance in ticks.
+     */
     public double convertToTicks(double inches) {
-        return (4096 / (6 * 3.1415926) * inches);
+        return (4096 / (RobotMap.wheelDiameter * 3.1415926) * inches);
     }
 
 }
