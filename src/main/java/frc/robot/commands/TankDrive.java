b4734dc178f5117d314be1ea5e4273013ab35fde
Mira Eisenhauer <vz.ceti.e@gmail.com>
2020-02-15 16:38:22 -0500

    Co-authored-by: Knaoinr <knaoinr@gmail.com>

--- src/main/java/frc/robot/commands/TankDrive.java
+++ src/main/java/frc/robot/commands/TankDrive.java
@@ -1,30 +1,37 @@
 package frc.robot.commands;
+
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.RobotMap;
 import frc.robot.subsystems.Subsystems;
 import frc.robot.userinterface.UserInterface;
 import io.github.pseudoresonance.pixy2api.*;
+
 /**
  * Uses joystick values to drive the bot in teleop.
  */
 public class TankDrive extends Command {
+
     private double updatedSpeed = 0;
     private double updatedRotation = 0;
-    private static final double maxChange = 0.5; //maxChange is acceleration
-    private int frameWidth = Subsystems.pixy.getFrameWidth();
+    private final double maxChange = 0.5; //maxChange is acceleration
+    private final int frameWidth;
+
     public TankDrive() {
         super("TankDrive");
         requires(Subsystems.driveBase);
+
+        frameWidth = Subsystems.pixy.getFrameWidth();
     }
+
     protected void initialize() {}
+
     protected void execute() {
         double speed;
         double rotation;
         Pixy2CCC.Block block = Subsystems.pixy.getBiggestBlock();
 
-        if (UserInterface.operatorController.getBumper(Hand.kLeft)) {
-            
+        if (UserInterface.driverController.A.get()) {
             if (block != null) {
                 System.out.println(block);
                 if (block.getX() > (frameWidth / 2)) {
@@ -32,20 +39,26 @@ public class TankDrive extends Command {
                 } else if (block.getX() < (frameWidth / 2)) {
                     Subsystems.driveBase.setMotors(0.1, 0); //consider adding speed to left motors
                 } else {
-                    if (block.getWidth() < 20){
+                    if (block.getWidth() < 20) {
                         Subsystems.driveBase.setMotors(0.1, 0.1);
                         System.out.println("Small boi");
                     }
                 }
+                System.out.println(block);
                 return;
+            } else {
+                System.out.println("No blocks found");
             }
-            
-        }
-        if (Subsystems.pixy.getBiggestBlock() != null) {
-            System.out.println("Yes, block!");
-            System.out.println(Subsystems.pixy.getBiggestBlock());
         } else {
-            System.out.println("Nooo!");
+            //TODO: if count < 5
+            //Sets the amount the controller rumbles when near a ball
+            try {
+                double blockSize = Subsystems.pixy.getBiggestBlock().getWidth();
+                double rumbleFactor = blockSize/frameWidth;
+                UserInterface.driverController.setRumble(rumbleFactor);
+            } catch (java.lang.NullPointerException e) {
+                UserInterface.driverController.setRumble(0);
+            }
         }
         
         /* Sets throttle for driveBase to the left stick Y-axis and sets the rotation
@@ -78,10 +91,11 @@ public class TankDrive extends Command {
             } else if (rotationDifference < -maxChange) {
             rotation = updatedRotation - maxChange;
         }
-        /** Because of a weird glitch with how curvatureDrive is set up,
+
+        /*  Because of a weird glitch with how curvatureDrive is set up,
          *  the rotation actually goes in as the first input, followed by the speed,
          *  rather than speed then rotation */
-            Subsystems.driveBase.cheesyDrive.curvatureDrive(RobotMap.getRotationCap() * rotation, RobotMap.getSpeedCap() * speed, true);
+        Subsystems.driveBase.cheesyDrive.curvatureDrive(RobotMap.getRotationCap() * rotation, RobotMap.getSpeedCap() * speed, true);
     }
     protected boolean isFinished() {
         return false;
