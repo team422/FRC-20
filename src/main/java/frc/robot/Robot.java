422926704cf80b1011f50f81c3cf63ae2441ee06
Dendendelen <d.y.plotnikov@gmail.com>
2020-02-14 19:41:41 -0500

    Added basic LED functionality through the PWM

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,6 +5,10 @@ import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import frc.robot.userinterface.*;
 import frc.robot.subsystems.*;
 import frc.robot.commands.*;
@@ -17,8 +21,13 @@ import io.github.pseudoresonance.pixy2api.*;
 public class Robot extends TimedRobot {
 
     private UsbCamera camera;
-
+    private AddressableLED led;
+    private AddressableLEDBuffer ledBuffer;
     private CenterTrench autonomous;
+    private Alliance currentAlliance;
+    int ledTimer = 0;
+    
+
 
     public Robot() {
         super(0.06);
@@ -27,6 +36,13 @@ public class Robot extends TimedRobot {
     public void robotInit() {
         RobotMap.setBot(RobotMap.BotNames.TOASTER);
         System.out.println("Initializing " + RobotMap.botName + "\n");
+        
+        led = new AddressableLED(0);
+        ledBuffer = new AddressableLEDBuffer(96);
+        led.setLength(ledBuffer.getLength());
+
+        led.setData(ledBuffer);
+        led.start();
 
         camera = CameraServer.getInstance().startAutomaticCapture();
 
@@ -37,6 +53,12 @@ public class Robot extends TimedRobot {
     public void disabledInit() {
         System.out.println("Disabled Initialized");
         Scheduler.getInstance().removeAll();
+        
+        for(int i = 0; i<ledBuffer.getLength(); i++){
+            ledBuffer.setRGB(i, 0, 178, 0);
+        }
+        ledBuffer.setRGB(95, 178, 0, 0);
+        led.setData(ledBuffer);
     }
 
     public void disabledPeriodic() {
@@ -61,6 +83,8 @@ public class Robot extends TimedRobot {
         System.out.println("TeleOp Initalized");
         Scheduler.getInstance().removeAll();
         Subsystems.driveBase.zeroEncoderPosition();
+        currentAlliance = DriverStation.getInstance().getAlliance();
+        
     }
 
     public void teleopPeriodic() {
@@ -73,6 +97,28 @@ public class Robot extends TimedRobot {
         } else {
             Subsystems.flyboi.stopWheel();
         }
+
+        
+        if (currentAlliance == Alliance.Red){
+            for(int i = 0; i<ledBuffer.getLength(); i++){
+                if (i%10<5){
+                    ledBuffer.setRGB(i, 100, 0, 0);
+                }
+                else{
+                    ledBuffer.setRGB(i,0,100,0);
+                }
+            }
+        }
+        else if (currentAlliance == Alliance.Blue){
+            for (int i = 0; i < ledBuffer.getLength(); i++){
+                ledBuffer.setRGB(i, 0, 0, 100);
+            }
+        }
+
+
+        led.setData(ledBuffer);
+
+        ledTimer++;
     }
 
     /**
