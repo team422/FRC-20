966abf56973d738267443688b08dfebae0b48cfd
Knaoinr <knaoinr@gmail.com>
2020-03-05 18:43:19 -0500

    changes

--- src/main/java/frc/robot/subsystems/Flyboi.java
+++ src/main/java/frc/robot/subsystems/Flyboi.java
@@ -18,7 +18,7 @@ public class Flyboi extends Subsystem {
     public CANEncoder rightEncoder;
     private SimpleMotorFeedforward feedforward;
 
-    public double wheelSpeed = 0.760;
+    public double wheelSpeed = 0.760; //0.760
 
     public Flyboi() {
         super("Flyboi");
