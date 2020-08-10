d13b00c267d15cbd540c428f824d095915ca07df
k4rstm4n <quinnkast@gmail.com>
2020-08-10 13:17:54 -0400

    changed TrackObject to work with Pi and Webcam instead of Pixy

--- src/main/java/frc/robot/commands/TrackObject.java
+++ src/main/java/frc/robot/commands/TrackObject.java
@@ -2,18 +2,11 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.subsystems.Subsystems;
-import io.github.pseudoresonance.pixy2api.*;
-
+import edu.wpi.first.networktables.*;
 /**
  * Uses pixy to face towards a power cell and prepare to consume.
  */
-public class TrackObject extends Command {
-
-    private Pixy2CCC.Block biggestBlock;
-    private double blockX;
-    private double blockWidth;
-    private int frameWidth;
-    private int counter;
+public class TrackObject extends Command {   
 
     public TrackObject() {
         super("TrackObject");
@@ -24,35 +17,17 @@ public class TrackObject extends Command {
     public void initialize() {
         Subsystems.driveBase.zeroEncoderPosition();
         Subsystems.driveBase.zeroGyroAngle();
-
-        frameWidth = Subsystems.pixy.getFrameWidth();
+       
     }
 
     @Override
-    public void execute() {
-        // Attempts to get biggest block
-        try {
-            biggestBlock = Subsystems.pixy.getBiggestBlock();
-            blockX = biggestBlock.getX();
-            blockWidth = biggestBlock.getWidth();
-            counter = 0;
-        } catch (java.lang.NullPointerException e) {
-            //if no block found for 10 loops, stop motors
-            if (counter < 10) {
-                counter++;
-            } else {
-                Subsystems.driveBase.stopMotors();
-            }
-            return;
-        }
-
-        double blockCenter = (blockWidth/2)+blockX;
-        double correction = blockCenter-(frameWidth/2);
-        correction *= 0.00632911; //shrinks correction to a -1 to 1 value (calculated with width of 316)
-
-        if (Math.abs(correction) > 0.25) {
-            Subsystems.driveBase.setMotors(-0.25*correction, 0.25*correction);
-            Subsystems.pixy.setLED(255,255,0);
+    public void execute() { 
+        NetworkTableInstance ntinst = NetworkTableInstance.getDefault();
+        NetworkTable table = ntinst.getTable("cellTable");
+        NetworkTableEntry correction = table.getEntry("correction");
+        double correct = correction.getDouble(0);
+        if (Math.abs(correct) > 0.25) {
+            Subsystems.driveBase.setMotors(-0.25*correct, 0.25*correct);
         }
     }
 
